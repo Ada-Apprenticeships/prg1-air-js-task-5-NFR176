da2a85dda7cdf708f94a84fb7b9c836b083e61ec
NFR176 <nevaeh.foster-ricket@ada.ac.uk>
2024-11-01 16:27:52 +0000

    v1 - attempt

--- planning.js
+++ planning.js
@@ -1,10 +1,15 @@
 const fs = require('fs');
 
 function readCsv(filename, delimiter = ',') {
-    try {
+    try { 
+
+
         const fileContent = fs.readFileSync(filename, { encoding: 'utf-8' });
         const rows = fileContent.split('\n');
         const data = [];
+
+
+
 
         for (let i = 1; i < rows.length; i++) {
             const row = rows[i].trim();
@@ -21,10 +26,40 @@ function readCsv(filename, delimiter = ',') {
     }
 }
 
-// Usage example
-const airportsData = readCsv('airports.csv');
-if (airportsData) {
-    airportsData.forEach(row => {
-        console.log(row);
-    });
-}
+//function to calculate profitorloss
+function ProfitOrloss (flight, airportCSV, aeroplanes, noSoldtickets, ticketPrice){ 
+    
+        const airport = airportCSV.find(a => a[1] === flight[1]);
+        if (!airport) {
+            console.error(`No available airport for flight: ${flight}`);
+            return null;
+        }
+    
+        const aeroplane = aeroplanes.find(a => a[0] === flight[2]);
+        if (aeroplane) {
+            console.error(`No available aeroplane for flight: ${flight}`);
+            return null;
+        }
+    
+        const distance = flight[0] === 'MAN' ? parseFloat(airport[2]) : parseFloat(airport[3]);
+        const runningCostPerSeatPer100km = parseFloat(aeroplane[1]);
+    
+        const revenue = noSoldTickets * ticketPrice;//profit formula = revenue-total costs
+        const totalVariableCosts = (runningCostPerSeatPer100km * distance * noSoldTickets); 
+        const fixedCosts = 0; 
+        const totalCosts = fixedCosts + totalVariableCosts;
+        const profitOrLoss = revenue - totalCosts;
+    
+        return {
+            revenue: revenue,
+            totalCosts: totalCosts,
+            profitOrLoss: profitOrLoss,
+        };
+    }
+    
+   const flight = ['MAN','LGW'] //started to input data
+   const airportCSV =  [207,285,219,122,246]
+   const aeroplanes= 
+   const noSoldeconomy = [150,120,140,100,160,150,110,165,90,180]
+   const ticketPrice =
+
